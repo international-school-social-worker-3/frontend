b3af7a488efa5b794aef98ac21d1ef9311e0a37a
Daniel Fieker <danielmfieker@gmail.com>
2020-02-03 17:58:26 -0600

    Finishing App.js and Components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import Login from './Components/Login';
 
 function App() {
+  const [data, setData] = useState([]);
+  console.log(data);
+  useEffect(() => {
+    axios
+      .get(
+        "http://localhost:5000/api/login"
+      )
+      .then(response => {
+        console.log(response.data.results);
+        setData(response.data.results);
+      })
+      .catch(error => {
+        console.log("No data returned", error);
+      });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Login />
     </div>
   );
 }
